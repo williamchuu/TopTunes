00d2e4e6e4e81725172fd156156c2c581f7d9649
William <wchu41@my.bcit.ca>
2023-02-10 00:46:10 -0800

    replaced styled-components with regular css

--- components/artists.js
+++ components/artists.js
@@ -2,47 +2,21 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
-import styled from 'styled-components'
 import songs from '../data/spotify.json'
 
 const inter = Inter({ subsets: ['latin'] })
 
-const Title = styled.div`
-display: flex;
-font-size: 16px;
-font-weight: 600;
-padding: 0 50px 0 50px;
-color: white;
-`
-
-const TitleCont = styled.div`
-display: flex;
-flex-direction: row;
-justify-content: space-between;
-align-items: center;
-padding: 18px 0px 18px 0px;
-`
-
-const Line = styled.hr`
-  border: 1px solid white;
-  width: 100%;
-`;
-
-const ThinLine = styled.hr`
-  border: 0.1px solid #5B5B5B;
-  width: 100%;
-`;
 export default function Artists() {
 
     return (
         <>
-            <TitleCont>
-                <Title>#</Title>
-                <Title>Song</Title>
-                <Title>Artist</Title>
-                <Title>Plays</Title>
-            </TitleCont>
-            <Line />
+            <div className={styles.titleCont}>
+                <div className={styles.title}>#</div>
+                <div className={styles.title}>Song</div>
+                <div className={styles.title}>Artist</div>
+                <div className={styles.title}>Plays</div>
+            </div>
+            <div className={styles.line} />
 
             {
                 songs.map((s, index) => {
@@ -54,7 +28,7 @@ export default function Artists() {
                                 <div className={styles.songArtist}>{s.Artist}</div>
                                 <div className={styles.songPlays}>{s.Streams.toLocaleString()} plays</div>
                             </div> : <></>}</>
-                            <ThinLine />
+                            <div className={styles.thinLine} />
                         </div>
                     )
                 })
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import songs from '../data/spotify.json'
-import Artists from './artists'
+import Artists from '../components/artists'
 
 const inter = Inter({ subsets: ['latin'] })
 
