a09353f5f19764d26cf6405cc4fb5416bac561ae
William <wchu41@my.bcit.ca>
2023-02-12 16:55:11 -0800

    logo redirects to home

--- components/logo.js
+++ components/logo.js
@@ -1,9 +1,15 @@
 import styles from '../styles/Home.module.css'
+import { useRouter } from 'next/router'
 
 export default function Logo() {
+    const router = useRouter();
+
+    const handleClickLogo = () => {
+        router.push('/')
+    }
     return (
         <div className={styles.logoCont}>
-            <img src="/toptuneslogo.svg" alt="logo" className={styles.logo} />
+            <img onClick={handleClickLogo} src="/toptuneslogo.svg" alt="logo" className={styles.logo} />
         </div>
     )
 }
